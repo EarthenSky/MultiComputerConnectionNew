c22b5bde3fb8d90a0f65bf5599be6531ece0a9fc
EarthenSky <127-gstang@sd43.bc.ca>
2017-05-17 10:11:50 -0700

    base code

---
+++ MultiComputerConnection/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lbxChatConsole = New System.Windows.Forms.ListBox()
+        Me.btnUpdateChatConsole = New System.Windows.Forms.Button()
+        Me.tbxMessageToSend = New System.Windows.Forms.TextBox()
+        Me.tbxConnectionComputerName = New System.Windows.Forms.TextBox()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.lbxComputersConnectedTo = New System.Windows.Forms.ListBox()
+        Me.lblInfo2 = New System.Windows.Forms.Label()
+        Me.tmrListenerUpdate = New System.Windows.Forms.Timer(Me.components)
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbxChatConsole
+        '
+        Me.lbxChatConsole.FormattingEnabled = True
+        Me.lbxChatConsole.Location = New System.Drawing.Point(12, 12)
+        Me.lbxChatConsole.Name = "lbxChatConsole"
+        Me.lbxChatConsole.Size = New System.Drawing.Size(468, 277)
+        Me.lbxChatConsole.TabIndex = 0
+        '
+        'btnUpdateChatConsole
+        '
+        Me.btnUpdateChatConsole.BackColor = System.Drawing.Color.Bisque
+        Me.btnUpdateChatConsole.FlatAppearance.BorderSize = 2
+        Me.btnUpdateChatConsole.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnUpdateChatConsole.Location = New System.Drawing.Point(486, 13)
+        Me.btnUpdateChatConsole.Name = "btnUpdateChatConsole"
+        Me.btnUpdateChatConsole.Size = New System.Drawing.Size(176, 40)
+        Me.btnUpdateChatConsole.TabIndex = 1
+        Me.btnUpdateChatConsole.Text = "Send Message"
+        Me.btnUpdateChatConsole.UseVisualStyleBackColor = False
+        '
+        'tbxMessageToSend
+        '
+        Me.tbxMessageToSend.Location = New System.Drawing.Point(486, 59)
+        Me.tbxMessageToSend.Multiline = True
+        Me.tbxMessageToSend.Name = "tbxMessageToSend"
+        Me.tbxMessageToSend.Size = New System.Drawing.Size(176, 32)
+        Me.tbxMessageToSend.TabIndex = 2
+        '
+        'tbxConnectionComputerName
+        '
+        Me.tbxConnectionComputerName.Location = New System.Drawing.Point(486, 224)
+        Me.tbxConnectionComputerName.Name = "tbxConnectionComputerName"
+        Me.tbxConnectionComputerName.Size = New System.Drawing.Size(176, 20)
+        Me.tbxConnectionComputerName.TabIndex = 3
+        Me.tbxConnectionComputerName.Text = "127-R300-C002"
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Location = New System.Drawing.Point(483, 208)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(179, 13)
+        Me.lblInfo.TabIndex = 4
+        Me.lblInfo.Text = "Input computer name to conenct to: "
+        '
+        'lbxComputersConnectedTo
+        '
+        Me.lbxComputersConnectedTo.FormattingEnabled = True
+        Me.lbxComputersConnectedTo.Location = New System.Drawing.Point(486, 110)
+        Me.lbxComputersConnectedTo.Name = "lbxComputersConnectedTo"
+        Me.lbxComputersConnectedTo.Size = New System.Drawing.Size(176, 95)
+        Me.lbxComputersConnectedTo.TabIndex = 5
+        '
+        'lblInfo2
+        '
+        Me.lblInfo2.AutoSize = True
+        Me.lblInfo2.Location = New System.Drawing.Point(483, 94)
+        Me.lblInfo2.Name = "lblInfo2"
+        Me.lblInfo2.Size = New System.Drawing.Size(159, 13)
+        Me.lblInfo2.TabIndex = 6
+        Me.lblInfo2.Text = "List of computers connected to: "
+        '
+        'tmrListenerUpdate
+        '
+        Me.tmrListenerUpdate.Enabled = True
+        '
+        'btnConnect
+        '
+        Me.btnConnect.BackColor = System.Drawing.Color.Bisque
+        Me.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnConnect.Location = New System.Drawing.Point(486, 250)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(176, 40)
+        Me.btnConnect.TabIndex = 7
+        Me.btnConnect.Text = "Connect To Computer"
+        Me.btnConnect.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Linen
+        Me.ClientSize = New System.Drawing.Size(674, 303)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.lblInfo2)
+        Me.Controls.Add(Me.lbxComputersConnectedTo)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.tbxConnectionComputerName)
+        Me.Controls.Add(Me.tbxMessageToSend)
+        Me.Controls.Add(Me.btnUpdateChatConsole)
+        Me.Controls.Add(Me.lbxChatConsole)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "MultiComputerConnection"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbxChatConsole As System.Windows.Forms.ListBox
+    Friend WithEvents btnUpdateChatConsole As System.Windows.Forms.Button
+    Friend WithEvents tbxMessageToSend As System.Windows.Forms.TextBox
+    Friend WithEvents tbxConnectionComputerName As System.Windows.Forms.TextBox
+    Friend WithEvents lblInfo As System.Windows.Forms.Label
+    Friend WithEvents lbxComputersConnectedTo As System.Windows.Forms.ListBox
+    Friend WithEvents lblInfo2 As System.Windows.Forms.Label
+    Friend WithEvents tmrListenerUpdate As System.Windows.Forms.Timer
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+
+End Class
---
+++ MultiComputerConnection/Form1.vb
@@ -0,0 +1,193 @@
+﻿Imports System.Net.Sockets
+Imports System.Net
+Imports System.Threading
+Imports System.Runtime.InteropServices
+Imports System.IO
+
+Public Enum Process
+    None
+    StringStart
+    AddComEnd
+    GiveComs
+    MessageEnd
+End Enum
+
+Public Class Form1
+    'Gabe Stang
+    '
+    'Connect 3 computers together
+    'Can connect multiple computers (3) together and can ping name to console.
+
+    Public Const chrStartProcessingText As Char = Chr(0)
+    Public Const chrAddComToConnectListEnd As Char = Chr(1)
+    Public Const chrGiveComs As Char = Chr(2)
+    Public Const chrMessageEnd As Char = Chr(3)
+
+
+    Private listener As New TcpListener(5019)
+    Private client As New TcpClient
+
+    Private lstComputers As New List(Of String)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim ListenerThread As New Thread(New ThreadStart(AddressOf Listening))
+        tbxConnectionComputerName.Text = My.Computer.Name
+        ListenerThread.Start()
+    End Sub
+
+    Private Sub Listening()  'starts listening for info from other com.
+        listener.Start()
+    End Sub
+
+    'Get info from other computers here.
+    Private Sub tmrListenerUpdate_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrListenerUpdate.Tick
+        Try
+            If listener.Pending = True Then  'If a Computer wants to send something.
+                client = listener.AcceptTcpClient() 'Accepts the "message".
+
+                ProcessInformation(client)
+
+            End If
+        Catch ex As Exception
+            Console.WriteLine(ex)
+            Dim Errorresult As String = ex.Message
+            MessageBox.Show(Errorresult & vbCrLf & vbCrLf & "???", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ProcessInformation(ByRef client As System.Net.Sockets.TcpClient)
+        Dim shtInfo As String = ""
+
+        Dim Reader As New StreamReader(client.GetStream())  'Start getting the other com info.
+
+        Dim currentProcess As Process = Process.None 'Current thing to do with the recieved information.
+
+        While Reader.Peek > -1  'Changes each character sent into a string and adds it to the 
+            Dim chrCurrent As Char = Convert.ToChar(Reader.Read())
+
+            'Set process if needed
+            If chrCurrent = chrStartProcessingText Then
+                currentProcess = Process.StringStart
+                Continue While
+
+            ElseIf chrCurrent = chrAddComToConnectListEnd Then
+                currentProcess = Process.AddComEnd
+
+            ElseIf chrCurrent = chrGiveComs Then
+                currentProcess = Process.GiveComs
+
+            ElseIf chrCurrent = chrMessageEnd Then
+                currentProcess = Process.MessageEnd
+
+            End If
+
+            If currentProcess = Process.StringStart Then 'Adds chars to be proccessed
+                shtInfo += chrCurrent
+
+            ElseIf currentProcess = Process.AddComEnd Then  'this is inefficient cause all coms have to give anmes and it repeats.
+                If lstComputers.Contains(shtInfo) = False Then
+                    lstComputers.Add(shtInfo)
+                    lbxComputersConnectedTo.Items.Add(shtInfo)
+
+                    Dim Writer As New StreamWriter(client.GetStream())
+                    GiveComNamesToFriends(Writer, shtInfo)
+
+                End If
+                shtInfo = String.Empty
+
+            ElseIf currentProcess = Process.GiveComs Then 'Other computer tells it's name, this computer gives any other names to that com.
+                If lstComputers.Count <> 0 Then 'If lstComputers has items in it, send them
+                    client = New TcpClient(shtInfo, 5019)
+                    Dim Writer As New StreamWriter(client.GetStream())
+
+                    For index As Short = 0 To lstComputers.Count - 1
+                        Writer.Write(chrStartProcessingText & lstComputers(index).ToString() & chrAddComToConnectListEnd)
+                        Writer.Flush()
+                    Next
+
+                End If
+                'then adds
+                lstComputers.Add(shtInfo)
+                lbxComputersConnectedTo.Items.Add(shtInfo)
+                shtInfo = String.Empty
+
+            ElseIf currentProcess = Process.MessageEnd Then
+                lbxChatConsole.Items.Add(shtInfo.ToString())
+                shtInfo = String.Empty
+
+            Else
+                lbxChatConsole.Items.Add(" No Understandable Message Header ")
+
+            End If
+
+        End While
+    End Sub
+
+    Private Sub btnUpdateChatConsole_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdateChatConsole.Click
+        Try
+            ConnectToComputersAndSendMessages()
+
+            lbxChatConsole.Items.Add(tbxMessageToSend.Text)
+            lbxChatConsole.Items.Add(My.Computer.Name)
+
+            tbxMessageToSend.Text = "Sent!"
+        Catch ex As Exception
+            Console.WriteLine(ex)
+            Dim Errorresult As String = ex.Message
+            MessageBox.Show(Errorresult & vbCrLf & vbCrLf & "Please Review Client Address", "Error Sending Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ConnectToComputersAndSendMessages()
+        For index As Short = 0 To lstComputers.Count - 1
+            client = New TcpClient(lstComputers(index), 5019)
+
+            'Sends the message.
+            Dim Writer As New StreamWriter(client.GetStream())
+            Writer.Write(chrStartProcessingText & tbxMessageToSend.Text & chrMessageEnd &
+                         chrStartProcessingText & My.Computer.Name & chrMessageEnd)
+            Writer.Flush()
+        Next
+    End Sub
+
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConnect.Click  'Connects to a computer
+        If lstComputers.Contains(tbxConnectionComputerName.Text) Then
+            Exit Sub
+        End If
+
+        client = New TcpClient(tbxConnectionComputerName.Text, 5019)
+
+        'Send my name and ask for it's computers
+        Dim Writer As New StreamWriter(client.GetStream())
+        Writer.Write(chrStartProcessingText & My.Computer.Name & chrGiveComs)
+        Writer.Flush()
+
+        GiveComNamesToFriends(Writer, tbxConnectionComputerName.Text)
+
+        'send it my computers.
+        For index As Short = 0 To lstComputers.Count - 1
+            If lstComputers(index).ToString = My.Computer.Name Then
+                Continue For  'Don't include my name
+            End If
+            Writer.Write(chrStartProcessingText & lstComputers(index).ToString & chrAddComToConnectListEnd)
+            Writer.Flush()
+        Next
+
+        lstComputers.Add(tbxConnectionComputerName.Text)
+        lbxComputersConnectedTo.Items.Add(tbxConnectionComputerName.Text)
+    End Sub
+
+    Private Sub GiveComNamesToFriends(ByRef Writer As StreamWriter, ByVal name As String)
+        'send my computers the name
+        For index As Short = 0 To lstComputers.Count - 1
+            If lstComputers(index).ToString = My.Computer.Name Then
+                Continue For  'Don't send it to my name
+            End If
+
+            client = New TcpClient(lstComputers(index).ToString, 5019)
+            Writer.Write(chrStartProcessingText & name & chrAddComToConnectListEnd)
+            Writer.Flush()
+        Next
+    End Sub
+
+End Class
