547b53caddb483ae2fe11aa530deed255d2d12e3
EarthenSky <127-gstang@sd43.bc.ca>
2017-06-09 11:47:59 -0700

    it now makes the lines but does nothing with them

--- MultiComputerConnection/CollisionMap.vb
+++ MultiComputerConnection/CollisionMap.vb
@@ -8,17 +8,31 @@
     Public val As Short
 End Structure
 
+Public Structure Line  'A Line has two points 
+    Sub New(ByVal pnt1 As Point, ByVal pnt2 As Point)
+        Me.pnt1 = pnt1
+        Me.pnt2 = pnt2
+    End Sub
+
+    Public pnt1 As Point
+    Public pnt2 As Point
+End Structure
+
 Public Class PosList  'Is a list but also has the bounds of the area it encloses.
     Sub New(ByVal lstPnt As List(Of PixelPoint), ByVal rect As Rectangle)
         Me.lstPnt = lstPnt
         Me.rect = rect
     End Sub
 
-    Public lstPnt As List(Of PixelPoint)
+    Public lstPnt As New List(Of PixelPoint)  'Holds the red pixel points
     Public rect As Rectangle
+
+    Public lstLines As New List(Of Line)  'holds collider lines
 End Class
 
+'When making a map put the highest R value at the start of a specific box, like a line through the box
 Public Class CollisionMap 'Instantiate to make a collider map of lines, call the collide method to check it's collision with a circle.
+
 
     Public lstLstSections As New List(Of PosList) 'Shit just got real
     Public arySectionsX As New List(Of Short)
@@ -64,12 +78,14 @@ Public Class CollisionMap 'Instantiate to make a collider map of lines, call the
             For jIndex As Short = 0 To colMap.Height - 1
                 If bmpCollision.GetPixel(index, jIndex).R > 0 Then
                     AddRedPixelToList(New PixelPoint(New Point(index, jIndex), bmpCollision.GetPixel(index, jIndex).R))
-                    Debug.Print("hmm")
+                    Debug.Print(bmpCollision.GetPixel(index, jIndex).R.ToString & ", is red")
                 End If
             Next
         Next
-    End Sub
 
+        MakeLines()
+
+    End Sub
 
     Public Sub AddRedPixelToList(ByVal pxPnt As PixelPoint)
         Dim shtListIndexX = 0
@@ -98,7 +114,48 @@ Public Class CollisionMap 'Instantiate to make a collider map of lines, call the
         Next
     End Sub
 
-    Public Sub Collision()
+    Public Sub MakeLines()
+        For index As Short = 0 To lstLstSections.Count - 1  'Each loop is making lines inside a section.
+            SortByVal(lstLstSections(index).lstPnt)
+            For jIndex As Short = lstLstSections(index).lstPnt.Count - 1 To 1 Step -1 'Each loop makes a line
+                lstLstSections(index).lstLines.Add(New Line(lstLstSections(index).lstPnt(jIndex).pnt, lstLstSections(index).lstPnt(jIndex - 1).pnt))
+            Next
+        Next
+    End Sub
+
+    Private Sub SortByVal(ByRef lst As List(Of PixelPoint))  'Yes.  Better than my other sort
+        'makes temp equal to the list box
+        Dim temp As New List(Of PixelPoint)
+        For index As Short = 0 To lst.Count - 1 Step 1
+            temp.Add(New PixelPoint(lst(index).pnt, lst(index).val))
+        Next
+
+        Dim lstShortTemp As New List(Of PixelPoint)
+        Dim shtSmallest As Short = Short.MaxValue
+        Dim index2 As Short = 0
+
+        While temp.Count - 1 >= 0
+            shtSmallest = Short.MaxValue
+            index2 = 0
+
+            For index As Short = 0 To temp.Count - 1 Step 1
+                If temp(index).val < shtSmallest Then
+                    shtSmallest = temp(index).val
+                    index2 = index
+                End If
+            Next
+
+            lstShortTemp.Add(New PixelPoint(temp(index2).pnt, temp(index2).val))
+            temp.RemoveAt(index2)
+        End While
+
+        For index As Short = 0 To lst.Count - 1 Step 1
+            lst(index) = lstShortTemp(index)
+        Next
+
+    End Sub
+
+    Public Sub Collision(ByVal pnt As Point)
 
     End Sub
 
